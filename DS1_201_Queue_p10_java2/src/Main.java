583707bc1af9400d8f87efd73ad6e0dc8e5975ca
minchjung <78589751+minchjung@users.noreply.github.com>
2021-04-11 08:33:56 +0900

    Data Structure1_201<Queue>p09_[오큰수]17298_java 시간통과
    스텍 자료구조 활용에는 변함이 없다.
     앞전에 큐 와 스택으로 조금 복잡하게 구현한 코드는 오히려 시간이 더 짧다.
     결국 출력문 과 예외 처리 활용에서 시간초과 와 런타임 에러로 고생했는데 .. 
    조금 아쉽다 정답처리가 알고리즘에 있는게 아닌것 같다..
    
     이 문제는 인덱스를 스텍 자료로 활용하는게 키가 아니다 
     0번째 부터 해를 찾을때까지 모두 탐색하는것이 아니라 해가 아니면 그 다음으로 넘어가고 
     넘어간 곳 에서 (큰값)해를 찾게 되면 다시 해를 못찾았던 곳을 역순으로 탐색해 지금의 해와 비교하는것  <--이것이 키 포인트
     문제의 조건이 해당 숫자 보다 큰 가장 왼쪽값이므로 성립이 된다.
     결국 하나의 인덱스를 고정하고 큰 값을 찾을때까지 탐색하는 것이 아니라 큰 값을 발견하게되면 큰 값을 고정 시키고
     거꾸로 탐색해주는 것이다
     결국 스택 자료의 핵심적인 부분을 활용하는 것 
     인덱스 넣는걸 몰라서 시간 초과 나거나 자료구조를 잘 못쓰고 있다고 생각하는것은 착각 인것 같다. 
    인덱스 대신 큐에 input 값을 담고 큐에서 스택으로 옮기면서 비교해주고 인덱스 추적용 값을 전역으로 선언해주면 문제 없다

--- DS1_201_Queue_p10_java2/src/Main.java
+++ DS1_201_Queue_p10_java2/src/Main.java
@@ -1,21 +1,10 @@
 //Data Structure1_201<Queue>p09_[오큰수]17298_java 시간초과 통과 
 import java.util.*;
-// 스텍 자료구조 활용에는 변함이 없다.
-// 앞전에 큐 와 스택으로 조금 복잡하게 구현한 코드는 오히려 시간이 더 짧다.
-// 결국 출력문 과 예외 처리 활용에서 시간초과 와 런타임 에러로 고생했는데 .. 조금 아쉽다 정답처리 자체가..
-// 이 문제는 인덱스를 스텍 자료로 활용하는게 키가 아니다 
-// 0번째 부터 해를 찾을때까지 모두 탐색하는것이 아니라 해가 아니면 그 다음으로 넘어가고 
-// 넘어간 곳 에서 (큰값)해를 찾게 되면 다시 해를 못찾았던 곳을 역순으로 탐색해 지금의 해와 비교하는것  <--이것이 키 포인트다 
-// 문제의 조건이 해당 숫자 보다 큰 가장 왼쪽값이므로 성립이 된다.
-// 결국 하나의 인덱스를 고정하고 큰값을 찾을때까지 탐색하는 것이 아니라 큰 값을 발견하게되면 큰 값을 고정 시키고
-// 거꾸로 탐색해주는 것이다
-// 결국 스택 자료의 핵심적인 부분을 활용하는 것 
-// 인덱스 넣는걸 몰라서 시간 초과 나거나 자료구조를 잘 못쓰고 있다고 생각하는것은 착각 인것 같다. 
 
 public class Main {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+		int n = sc.nextInt(); 
 		Stack<Integer> stack = new Stack<>();
 		int[] ans = new int[n];
 		int[] arr = new int[n];
@@ -24,12 +13,16 @@ public class Main {
 		}
 		stack.add(0);
 		int i =1;
-		while(i < n) {
+		// i 가 항상 stack의 값보다 1만 크다! 
+		//바로 옆의 값만 조회 비교하고 넘어간다.
+		while(i < n) {// 마지막 인덱스까지 돌린다.
 			while(!stack.isEmpty()) {
 				if(arr[stack.peek()] < arr[i]) {
-					ans[stack.pop()] = arr[i];
-				}else {break;}
-			}
+					ans[stack.pop()] = arr[i];//큰값이 나타면 stack 최상단 값과 거꾸로 비교
+				}else {break;}// 스택이 비거나, 스택값이 (크다고 고정한 비교)값 보다 더 크면 탈출한다
+			}// +1옆자리에 계속 작은값이면 stack에 쌓이게 된다. 
+			//위의 while문 조건에 맞으면 stack값을 털어준다. 
+			//결국 큰 값이 없으면 stack 에 쌓이게 되고, 아래 1번에서 정리해준다.  
 			stack.push(i);
 			i++;
 		}  
