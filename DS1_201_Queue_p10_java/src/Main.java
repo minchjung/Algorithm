fa431acce7dbed96ee718244a88243ee4a687170
minchjung <78589751+minchjung@users.noreply.github.com>
2021-04-11 07:34:30 +0900

    Data Structure1_201<Queue>p09_[오큰수]17298_java 시간초과2

--- DS1_201_Queue_p10_java/src/Main.java
+++ DS1_201_Queue_p10_java/src/Main.java
@@ -2,38 +2,29 @@
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		Queue<Integer> q1 = new LinkedList<>();
-		Queue<Integer> q2 = new LinkedList<>();
-		
 		int n = sc.nextInt();
- 
-		for(int i =0; i < n; i++) { 
-			int a = sc.nextInt();
-			q1.add(a); 
-		} 
-		while(!q1.isEmpty()) {
-			int now = q1.poll();
-			while (!q1.isEmpty()){
-				int nxt = q1.poll();
-				q2.offer(nxt);
-				if (nxt > now) {
-					System.out.print(nxt+" ");
-					while(!q2.isEmpty()) {
-						q1.offer(q2.poll());
-					}
-					break;
-				}
-				if(!q1.isEmpty()) {
-					System.out.print(-1+" ");
-					while(!q2.isEmpty()) {
-						q1.offer(q2.poll());
-					}
-					break;
-				}
-			}
+		Stack<Integer> stack = new Stack<>();
+		int[] ans = new int[n];
+		int[] arr = new int[n];
+		for(int i = 0 ; i < n ; i++) {
+			arr[i]=sc.nextInt();
+			ans[i]=-1;
 		}
-		System.out.print(-1);
+		stack.add(0);
+		int i =1;
+		while(i < n) {
+			while(!stack.isEmpty()) {
+				if(arr[stack.peek()] < arr[i]) {
+					ans[stack.pop()] = arr[i];
+				}else {break;}
+			}
+			stack.add(i);
+			i++;
+		}
+		for(int j =0; j<n; j++) {
+			System.out.print(ans[j]+" ");
+		}
 	}//main String
 }//Main
