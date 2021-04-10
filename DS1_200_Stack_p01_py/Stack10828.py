d5e10b692da4c1bc480b083eb0fe80911d4a7727
minchjung <78589751+minchjung@users.noreply.github.com>
2021-04-10 19:10:43 +0900

    DataStructure1_200_<Stack>p01_[스택]10828_py

---
+++ DS1_200_Stack_p01_py/Stack10828.py
@@ -0,0 +1,27 @@
+# BOJ:200_Data Strucuture1<Stack Algorithm>-#10828-Stack-스택p01 
+stack = [] 
+statement=[]
+n = int(input())
+for _ in range(n):
+    statement.append(input().split())
+
+for strList in statement : 
+    if len(strList)==2:
+        stack.append(int(strList[1]))
+    else:
+        if not stack:
+            if strList[0]=="size":
+                print(0)
+            elif strList[0]=="empty":
+                print(1)
+            else:
+                print(-1)
+        else:
+            if strList[0]=="size":
+                print(len(stack))
+            elif strList[0]=="empty":
+                print(0)
+            elif strList[0]=="pop":
+                print(stack.pop())
+            else:
+                print(stack[-1])
