5494ffd4429849aca2cf78d969bee6cf08dc2544
minchjung <78589751+minchjung@users.noreply.github.com>
2021-04-10 22:07:45 +0900

    Data Structure1_201<Queue>p09_[문자열뒤집기2]17413_py

--- DS1_200_Stack_p09_py/FlipWords2_17413.py
+++ DS1_200_Stack_p09_py/FlipWords2_17413.py
@@ -1,10 +1,11 @@
-# Data Structure1_201_<Stack>-#17413[문자열뒤집기2]_p09
+# Data Structure1_201<Queue>_[문자열뒤집기2]17413_p09_py
 from collections import deque 
 string =input()
-dqStr = deque(string) 
-dqAns= deque()
-stack = [] 
+dqStr = deque(string) # original input dq
+dqAns= deque() # dq for print <tag> out
+stack = [] # stack for print string in backward 
 
+# print function , check whether its Stack or Queue
 def printAns(check):
     global dqStr
     global dqAns 
@@ -20,7 +21,7 @@ def printAns(check):
             if tag !="<": 
                 print(end=" ")
             dqStr.appendleft(tag)
-
+# stack function contains the String to poll backward 
 def stackFunc(now):
     global dqStr
     global stack
@@ -29,16 +30,17 @@ def stackFunc(now):
         now = dqStr.popleft()
         if now != " " and now !="<":
             stack.append(now)
-        else:
-            if now =="<":
+        else: # string meets tag or " "space, then it ends to print
+            if now =="<": #"tag  must be into dqStr, again"
                 dqStr.appendleft(now)
                 printAns(1)
                 break      
             else:
                 printAns(1)
+    # if not <tag> and the String in the end, should be printed
     if stack: 
         printAns(1)
-
+# queue function contains the <tag> to print forward
 def deqFunc(now):
     global dqStr 
     global dqAns 
@@ -47,7 +49,7 @@ def deqFunc(now):
         now = dqStr.popleft()
         dqAns.append(now)
     printAns(-1)
-
+# execute to function with original input (dq-String)
 while dqStr:
     now = dqStr.popleft()
     if now =="<":
