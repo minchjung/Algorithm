aed2620cb324e595df50ec8a9c00c5a8dd855c0e
minchjung <78589751+minchjung@users.noreply.github.com>
2021-04-16 22:25:07 +0900

    000<String2>_p06_[Print_as_it_is2]11719_java

---
+++ DS0_000_String2_p06_java/src/Main.java
@@ -0,0 +1,11 @@
+// 000<String2>_p06_[그대로출력2]11719_java
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner sc = new Scanner(System.in); 
+        while(sc.hasNextLine()){
+            System.out.println(sc.nextLine());
+        }
+    }
+}
