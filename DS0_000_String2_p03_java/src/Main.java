dafe3a7c8717dd88e947c8b89cdcb091f8412a90
minchjung <78589751+minchjung@users.noreply.github.com>
2021-04-16 22:22:30 +0900

    000<String2>p03_[Print_as_it_is]11718_java

---
+++ DS0_000_String2_p03_java/src/Main.java
@@ -0,0 +1,10 @@
+// 000<String2>p03_[그대로출력하기]11718_java
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in); 
+        while(sc.hasNext()){
+            System.out.println(sc.nextLine());
+        }
+    }
+}
