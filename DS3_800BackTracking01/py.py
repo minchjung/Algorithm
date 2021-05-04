07a5893bc49362fdb5e5e36b8598d450503b4ba8
minchjung <78589751+minchjung@users.noreply.github.com>
2021-05-05 00:03:59 +0900

    800 BackTracking[S3]15649_py

---
+++ DS3_800BackTracking01/py.py
@@ -0,0 +1,19 @@
+# 800 BackTracking[S3]15649_py
+N,M= map(int, input().split())
+isUsed=[False]*(N+1) 
+comb=[0]*(M) 
+
+def gogo(k):
+    if k==(M): 
+        print(*comb) 
+        return 
+        # BackTracking 
+        # When it meets the return condition above,
+        # then it make the one to be original position 
+    for i in range(1,N+1):
+        if isUsed[i] :continue
+        comb[k] = i 
+        isUsed[i]=True 
+        gogo(k+1) # return here and  
+        isUsed[i]=False # <-- back in the place     
+gogo(0)
