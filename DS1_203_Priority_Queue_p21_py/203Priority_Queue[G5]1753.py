8ce695a2191fa81fa7c27d56ab2f66241c22a0a0
minchjung <78589751+minchjung@users.noreply.github.com>
2021-05-09 19:31:26 +0900

    203Priority_Queue[G5]1753 시간초과 겨우통과 (912ms/1s)

---
+++ DS1_203_Priority_Queue_p21_py/203Priority_Queue[G5]1753.py
@@ -0,0 +1,28 @@
+#  시간 초과 .. 해결 
+import heapq 
+V,E =map(int, input().split())
+start =int(input())
+board=[[] for _ in range(V+1)] 
+isUsed=[11*E]*(V+1)
+for _ in range(E):
+    a,b,c=map(int, input().split())
+    board[a].append([c,b])
+hq=[[0,start]] 
+isUsed[start]=0
+heapq.heapify(hq)
+while hq: 
+    now = heapq.heappop(hq) #now=[가중치, 노드번호] 가중치가 작은순으로 pop 된다
+    if isUsed[now[1]] < now[0]: continue # 입구컷 
+    for nodePair in board[now[1]]: # now 노드에 연결된 [가중치,다른노드] 정보를 탐색
+        if isUsed[nodePair[1]] <= isUsed[now[1]] + nodePair[0]: continue 
+        # 만약 now노드 거쳐 탐색 노드로 갔을때, 기존의 거리보다 더 크면 입구컷
+        isUsed[nodePair[1]] = isUsed[now[1]] + nodePair[0]
+        # 더 작을때만 그 가중치 거리를 메모에 저장하고 탐색한 노드 정보[가중치, 노드번호]를 hq에 담아준다   
+        heapq.heappush(hq, [ isUsed[nodePair[1]], nodePair[1]]) #<-- 위처럼 탐색 노드 정보를 그대로 넣어주지 않고, 바뀌면 바뀐 거리정보로 
+        # 해당 노드까지의 최단거리 가중치 거리를 갱신해서 힙자료에 넣어주면 조금더 입구컷이 빨라진다  
+        # 원래 코드 : heapq.heappush(hq, nodePair)
+        # 힙push 돌때마다 가중치가 작은 노드 순으로 정렬되 while을 반복한다 
+for i in range(1,V+1):
+    if isUsed[i]==11*E: print("INF")
+    else: print(isUsed[i])
+# 912ms 겨우 통과..;
