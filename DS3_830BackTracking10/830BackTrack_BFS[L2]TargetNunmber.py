25813f084387b634f1a6fde8626fae80c635ca47
minchjung <78589751+minchjung@users.noreply.github.com>
2021-05-12 19:00:29 +0900

    830BackTrack_BFS[L2]TargetNunmber

---
+++ DS3_830BackTracking10/830BackTrack_BFS[L2]TargetNunmber.py
@@ -0,0 +1,13 @@
+# 931BackTrack_BFS[L2]TargetNunmber_py
+def solution(numbers,target): 
+    cnt=0
+    def backTrack(k, tot):
+        nonlocal target
+        nonlocal cnt 
+        if k == len(numbers): 
+            if tot == target : cnt+=1 
+            return 
+        backTrack(k+1,tot+numbers[k])
+        backTrack(k+1,tot-numbers[k])
+    backTrack(0,0)
+    return cnt 
