0699ca772556c365b5f1ac464a2fc1c60eafa37b
Minchan <78589751+minchjung@users.noreply.github.com>
2021-08-16 04:52:59 +0900

    Programmers 부족한 금액 계산하기

--- py copy.py
+++ py copy.py
@@ -1,7 +1,6 @@
-
-def solution(brown, yellow):
-    tot = brown //2 
-    w = tot 
-    while w and (w-2)*(tot-w) !=yellow:  w-=1 
-    
-    return [w,tot-w+2]
+def solution(price,money,count):
+    arr = [price*i for i in range(count+1)]
+    ans =sum(arr[:count+1]) - money 
+    if ans <= 0 : ans =0 
+    return ans 
+solution(3,20,4)
