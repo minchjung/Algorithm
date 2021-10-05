9b3ca0be3fdc0d8565fb7bbb5e2bdcd940790c8a
Minchan <78589751+minchjung@users.noreply.github.com>
2021-10-05 23:03:43 +0900

    Update 830BackTracking[G5]9663_N-Queen.py

--- DS3_830BackTracking09/830BackTracking[G5]9663_N-Queen.py
+++ DS3_830BackTracking09/830BackTracking[G5]9663_N-Queen.py
@@ -5,8 +5,8 @@ N = int(input())
 isUsedY=[0]*N  # valid indicator of the column, y   
 isUsedCross1 = [0]*(30) # this indicates the dignole across the map uppper to low 
 isUsedCross2 = [0]*(30) # lower to upper 
-cnt=0 # if function reaches to last row(x=N), that's the case where the Quenes spotted it all 
-#  x=N<-- out of the range, but thats the condition to escape reculsive,here 
+cnt=0 # if function reaches to last row(x=N), that's the case where the Quenes all spotted 
+#  x=N<-- out of the range, but thats the condition to escape recursion,here 
 def DFS(x):
     global cnt 
     if x==N:
@@ -19,7 +19,7 @@ def DFS(x):
         isUsedCross1[x+y]=1  
         isUsedCross2[x-y+N-1]=1
         DFS(x+1) # go to the next row
-        # reculsive gets back to here if it meets all condition, 
+        # it recursively gets back to here if it meets all condition, 
         # but we need to see if it has more valid answer over the rest of the For-Loop (more culmn on that row)
         # So it is necessary to make each valid indicator is back to valid again
         isUsedY[y]=0   
